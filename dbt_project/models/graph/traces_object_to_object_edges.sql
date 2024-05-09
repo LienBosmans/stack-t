e97d6b3a2ebf12e8416b7edf75c45d031bc80736
Lien <lienbosmans@live.com>
2024-05-09 23:45:31 +0200

    Fixed missing object-to-object relations in graph visualizations

--- dbt_project/models/graph/traces_object_to_object_edges.sql
+++ dbt_project/models/graph/traces_object_to_object_edges.sql
@@ -10,10 +10,14 @@ with object_to_object_edges as (
         target_object_snapshots.object_id as target_object_id
     from
         {{ ref('object_snapshots') }} as source_object_snapshots
-        ASOF inner join {{ ref('object_to_object') }}
+        inner join {{ ref('object_to_object_windows') }} as object_to_object
             on (
                 source_object_snapshots.object_id = object_to_object.source_object_id
-                and source_object_snapshots.snapshot_timestamp >= object_to_object.timestamp
+                and source_object_snapshots.snapshot_timestamp >= object_to_object.valid_from_timestamp
+                and (
+                    source_object_snapshots.snapshot_timestamp < object_to_object.valid_til_timestamp
+                    or object_to_object.valid_til_timestamp is null
+                )
             )
         inner join {{ ref('object_snapshots') }} as target_object_snapshots
             on (
