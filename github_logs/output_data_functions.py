f6c55f0696e24b9a5cd7a98a57c5937690846712
Lien <lienbosmans@live.com>
2025-01-03 16:13:24 +0100

    Added writing empty tables and casting columns to correct types

--- github_logs/output_data_functions.py
+++ github_logs/output_data_functions.py
@@ -25,11 +25,52 @@ def dataframe_to_persistent_duckdb(df_records:pl.DataFrame,table_name:str,duckdb
     drop table if exists ''' + table_name + '''
     ''')
 
-    # write table to database
+    # check if empty table needs to be generated
     if not df_records.is_empty():
-        con.sql('''--sql
-        create table  ''' + table_name + ''' as       
-            select * from df_records
-        ''')
+        select_statement = get_sql_statement(table_name,is_empty_table=False)
+    else:
+        select_statement = get_sql_statement(table_name,is_empty_table=True)
+    
+    # write table to database
+    con.sql('''--sql
+    create table  ''' + table_name + ''' as       
+        ''' + select_statement + '\n'
+    )
 
     return None
+
+def get_sql_statement(table_name:str,is_empty_table:bool,df_name:str="df_records"):
+    """Used to generate an empty table in case the dataframe is empty,
+    or to cast all columns to the correct datatype."""
+
+    if table_name == 'event_types':
+        columns = [['id',  'INTEGER'],  ['description','VARCHAR']]
+    elif table_name == 'events':
+        columns = [['id','INTEGER'],  ['event_type_id', 'INTEGER'], ['timestamp','TIMESTAMP'], ['description','VARCHAR']]
+    elif table_name == 'event_attributes':
+        columns = [['id','INTEGER'],  ['event_type_id', 'INTEGER'], ['description','VARCHAR'], ['datatype','VARCHAR']]
+    elif table_name == 'event_attribute_values':
+        columns = [['id','INTEGER'],  ['event_id','INTEGER'], ['event_attribute_id','INTEGER'],['attribute_value','VARCHAR']]
+    elif table_name == 'object_types':
+        columns = [['id','INTEGER'],  ['description','VARCHAR']]
+    elif table_name == 'objects':
+        columns = [['id','INTEGER'],  ['object_type_id','INTEGER'], ['description','VARCHAR']]
+    elif table_name == 'object_attributes':
+        columns = [['id','INTEGER'],  ['object_type_id','INTEGER'], ['description','VARCHAR'], ['datatype','VARCHAR']]
+    elif table_name == 'object_attribute_values':
+        columns = [['id','INTEGER'],  ['object_id','INTEGER'], ['object_attribute_id','INTEGER'], ['timestamp','TIMESTAMP'], ['attribute_value','VARCHAR']]
+    elif table_name == 'event_to_object':
+        columns = [['id','INTEGER'],  ['event_id','INTEGER'], ['object_id','INTEGER'], ['qualifier_id','INTEGER'], ['qualifier_value','VARCHAR']]
+    elif table_name == 'event_to_object_attribute_value':
+        columns = [['id','INTEGER'],  ['event_id','INTEGER'], ['object_attribute_value_id','INTEGER'], ['qualifier_id','INTEGER'], ['qualifier_value','VARCHAR']]
+    elif table_name == 'object_to_object':
+        columns = [['id','INTEGER'],  ['source_object_id','INTEGER'], ['target_object_id','INTEGER'], ['qualifier_id','INTEGER'], ['timestamp','TIMESTAMP'], ['qualifier_value','VARCHAR']]
+    elif table_name == 'relation_qualifiers':
+        columns = [['id','INTEGER'],  ['description','VARCHAR'], ['datatype','VARCHAR']]
+    
+    if is_empty_table:
+        select_statement = "select " + ",".join([f"null::{column[1]} as {column[0]}" for column in columns]) + " where 1!=1"
+    else:
+       select_statement = "select " + ",".join([f"{column[0]}::{column[1]} as {column[0]}" for column in columns]) + f" from {df_name}"
+
+    return select_statement
